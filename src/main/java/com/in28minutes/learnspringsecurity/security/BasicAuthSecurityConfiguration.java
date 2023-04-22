fb77226b4619022225b292cf88d109679398c850
evgen <parom.evgen@mail.ru>
2023-04-22 23:40:56 +0300

    disable CSRF, we do not need send _csrfToken every request Session stateless, we do not have any session anymore login and logout do not available now

---
+++ src/main/java/com/in28minutes/learnspringsecurity/security/BasicAuthSecurityConfiguration.java
@@ -0,0 +1,25 @@
+package com.in28minutes.learnspringsecurity.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class BasicAuthSecurityConfiguration {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> {
+            auth.anyRequest().authenticated();
+        });
+//        http.formLogin();
+        http.sessionManagement(
+                session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
+        http.httpBasic();
+        http.csrf().disable();
+        return http.build();
+    }
+}
