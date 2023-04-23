94288847e610c77b4729f7be84571d0979678d57
evgen <parom.evgen@mail.ru>
2023-04-23 08:45:33 +0300

    create Users in memory

--- src/main/java/com/in28minutes/learnspringsecurity/security/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/in28minutes/learnspringsecurity/security/BasicAuthSecurityConfiguration.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,12 +18,18 @@ public class BasicAuthSecurityConfiguration {
         http.authorizeHttpRequests(auth -> {
             auth.anyRequest().authenticated();
         });
-//        http.formLogin();
         http.sessionManagement(
                 session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         );
         http.httpBasic();
         http.csrf().disable();
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user = User.withUsername("in28minutes").password("{noop}dummy").roles("USER").build();
+        UserDetails admin = User.withUsername("admin").password("{noop}dummy").roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
