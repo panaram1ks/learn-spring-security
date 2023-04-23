d8a528a7f9d92abc5302f56e56b612f627e6098e
evgen <parom.evgen@mail.ru>
2023-04-23 18:06:56 +0300

    create Jwt Token, and send it in request body

--- src/main/java/com/in28minutes/learnspringsecurity/jwt/JwtAuthenticationResource.java
+++ src/main/java/com/in28minutes/learnspringsecurity/jwt/JwtAuthenticationResource.java
@@ -1,14 +1,47 @@
 package com.in28minutes.learnspringsecurity.jwt;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+import java.util.stream.Collectors;
 
 @RestController
 public class JwtAuthenticationResource {
 
-    @PostMapping("/authenticate")
-    public Authentication authenticate(Authentication authentication) {
-        return authentication;
+    private JwtEncoder jwtEncoder;
+
+    public JwtAuthenticationResource(JwtEncoder jwtEncoder) {
+        this.jwtEncoder = jwtEncoder;
     }
+
+    @PostMapping("/authenticate")
+    public JwtResponse authenticate(Authentication authentication) {
+        return new JwtResponse(createToken(authentication));
+    }
+
+    private String createToken(Authentication authentication) {
+        JwtClaimsSet claimsSet = JwtClaimsSet.builder()
+                .issuer("self")
+                .issuedAt(Instant.now())
+                .expiresAt(Instant.now().plusSeconds(60 * 10))
+                .subject(authentication.getName())
+                .claim("scope", createScope(authentication))
+                .build();
+
+        JwtEncoderParameters parameters = JwtEncoderParameters.from(claimsSet);
+        return jwtEncoder.encode(parameters).getTokenValue();
+    }
+
+    private String createScope(Authentication authentication) {
+        return authentication.getAuthorities().stream().map(auth -> auth.getAuthority()).collect(Collectors.joining(" "));
+    }
+
+}
+
+record JwtResponse(String token) {
 }
