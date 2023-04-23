187f4ced50ecce98f3e29237915babf7b20e3be5
evgen <parom.evgen@mail.ru>
2023-04-23 09:23:53 +0300

    JdbcUserDetailsManager create users in database

--- src/main/java/com/in28minutes/learnspringsecurity/security/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/in28minutes/learnspringsecurity/security/BasicAuthSecurityConfiguration.java
@@ -2,13 +2,20 @@ package com.in28minutes.learnspringsecurity.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+import static org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION;
 
 @Configuration
 public class BasicAuthSecurityConfiguration {
@@ -23,13 +30,30 @@ public class BasicAuthSecurityConfiguration {
         );
         http.httpBasic();
         http.csrf().disable();
+
+        //problem with showing frame in /h2-console and we solve it
+        http.headers().frameOptions().sameOrigin();
+
         return http.build();
     }
 
     @Bean
-    public UserDetailsService userDetailsService(){
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript(DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
         UserDetails user = User.withUsername("in28minutes").password("{noop}dummy").roles("USER").build();
-        UserDetails admin = User.withUsername("admin").password("{noop}dummy").roles("ADMIN").build();
-        return new InMemoryUserDetailsManager(user, admin);
+        UserDetails admin = User.withUsername("admin").password("{noop}dummy").roles("ADMIN", "USER").build();
+
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.createUser(user);
+        jdbcUserDetailsManager.createUser(admin);
+
+        return jdbcUserDetailsManager;
     }
 }
